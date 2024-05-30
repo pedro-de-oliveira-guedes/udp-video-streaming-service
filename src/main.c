068d1f2dc7df2f6dc670b5ef6a2c3cf0c1f1e71c
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2024-05-30 18:55:25 -0300

    Feat: Testing movie TAD.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
+#include "movie.h"
+
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    printf("Hello, World!\n");
+    char *script[5] = {
+        "Hello, world!",
+        "I'm a movie script.",
+        "I'm a movie script.",
+        "I'm a movie script.",
+        "I'm a movie script."
+    };
+
+    Movie *movie = create_movie("My Movie", script);
+    run_movie(movie);
+
     return 0;
 }
