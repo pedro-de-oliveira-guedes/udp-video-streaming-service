f0f6edec37f859a31ae5c8bf203ed9c5ae056b85
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2024-06-22 15:51:48 -0300

    feat: implementing streaming server methods

---
+++ src/streaming_server.c
@@ -0,0 +1,79 @@
+#include "streaming_server.h"
+
+#include <unistd.h>
+
+#define MAX_TITLE_SIZE 100
+#define MAX_SCRIPT_LINE_SIZE 200
+
+StreamingServer* createStreamingServer(int argc, char **argv) {
+    Server *server = parseArgumentsAndCreateServer(argc, argv);
+
+    StreamingServer *streamingServer = (StreamingServer *) malloc(sizeof(StreamingServer));
+    streamingServer->server = server;
+    streamingServer->catalog = createCatalog();
+
+    return streamingServer;
+}
+
+void communicateWithClient(StreamingServer *streamingServer, int clientSocket) {
+    if (provideCatalogToClient(streamingServer, clientSocket) != 0) {
+        logError("Erro ao enviar o catálogo para o cliente.");
+    }
+
+    if (handleClientRequests(streamingServer, clientSocket) != 0) {
+        logError("Erro ao lidar com as requisições do cliente.");
+    }
+}
+
+int provideCatalogToClient(StreamingServer *streamingServer, int clientSocket) {
+    for (int i = 0; i < CATALOG_SIZE; i++) {
+        int movieId = i + 1;
+        if (send(clientSocket, &movieId, sizeof(int), 0) == -1) {
+            return -1;
+        }
+
+        char movieTitle[MAX_TITLE_SIZE];
+        strcpy(movieTitle, streamingServer->catalog->movies[i]->title);
+        if (send(clientSocket, &movieTitle, sizeof(movieTitle), 0) == -1) {
+            return -1;
+        }
+    }
+
+    int endOfCatalog = -1;
+    if (send(clientSocket, &endOfCatalog, sizeof(int), 0) == -1) {
+        return -1;
+    }
+
+    return 0;
+}
+
+int movieIdIsValid(int movieId) {
+    return movieId >= 1 && movieId <= CATALOG_SIZE;
+}
+
+int handleClientRequests(StreamingServer *streamingServer, int clientSocket) {
+    while (1) {
+        int movieId;
+        if (recv(clientSocket, &movieId, sizeof(int), 0) == -1) {
+            return -1;
+        }
+        if (!movieIdIsValid(movieId)) {
+            break;
+        }
+
+        Movie *movie = streamingServer->catalog->movies[movieId - 1];
+        for (int i = 0; i < SCRIPT_LINES; i++) {
+            if (send(clientSocket, movie->script[i], MAX_SCRIPT_LINE_SIZE, 0) != 0) {
+                return -1;
+            }
+        }
+    }
+
+    return 0;
+}
+
+void closeStreamingServer(StreamingServer *streamingServer) {
+    closeServer(streamingServer->server);
+    freeCatalog(streamingServer->catalog);
+    free(streamingServer);
+}
